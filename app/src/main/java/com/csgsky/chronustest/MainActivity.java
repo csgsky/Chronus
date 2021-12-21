accfc8de916dd620057c8f6d108f182000223e36
陈少罡 <674143140@qq.com>
2021-12-21 16:53:22 +0800

    test

--- app/src/main/java/com/csgsky/chronustest/MainActivity.java
+++ app/src/main/java/com/csgsky/chronustest/MainActivity.java
@@ -3,6 +3,17 @@ package com.csgsky.chronustest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.csgsky.cscommon.MessageMonitor;
+import com.csgsky.cscommon.MessageMonitorBuilder;
+import com.csgsky.cscommon.MonitorCallback;
+import com.csgsky.cscommon.MonitorConfig;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +22,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.start);
+        start.setOnClickListener(v -> {
+            Log.d("MessageMonitorLog", "--------start----------------");
+            MonitorConfig monitorConfig = MessageMonitorBuilder.startBuilder()
+                    .setCatDuration(2000L)
+                    .setSaveCount(500)
+                    .setMinLagTime(100L)
+                    .setMinMessageCost(1000L)
+                    .setIdleClear(false).build();
+            MessageMonitor.startMonitor(monitorConfig);
+        });
+
+        Button slow = findViewById(R.id.slow);
+        slow.setOnClickListener(v -> {
+            try {
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(this, "123", Toast.LENGTH_SHORT).show();
+        });
+        Button test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mainReportMessageData = MessageMonitor.INSTANCE.getMainReportMessageData();
+                Log.d("MainData", mainReportMessageData);
+            }
+        });
+
+        MessageMonitor.setMonitorCallBack(new MonitorCallback() {
+            @Override
+            public void reportLagMessages(@NotNull String errorType, @NotNull String messages) {
+                Log.i(errorType, "reportLagMessages: " + messages);
+            }
+
+            @Override
+            public void monitorError(@NotNull String errorType, @NotNull String errorLog) {
+                Log.i("MessageMonitor", "errorType: " + errorType + " , errorLog: " + errorLog);
+            }
+        });
+
     }
 }
--- chronus-common/src/main/java/com/csgsky/cscommon/storage/DefaultShouldRemoveDelegate.kt
+++ chronus-common/src/main/java/com/csgsky/cscommon/storage/DefaultShouldRemoveDelegate.kt
@@ -16,10 +16,8 @@ class DefaultShouldRemoveDelegate : SuperCache.ShouldRemoveDelegate<String, Inne
     ): Boolean {
         // 消息数小于最大数量
         if (cache.innerCache.size <= MessageMonitor.getMainInnerData()?.monitorConfig?.saveCount ?: 100) {
-            Log.d("MessageMonitorLog", "--single--message---------------------end-------------------------\n-")
             return false
         }
-        Log.d("MessageMonitorLog", "--single--message----------------------------------------------\n-")
         // 消息中存在卡顿
         return true
     }
